bb038185a06448de4a41dd793cb6c0cf6983ee8f
JesusPortillo <65827936+JesusPortillo@users.noreply.github.com>
2021-09-02 12:06:52 -0500

    Clase nevera desarrollada

--- src/main/java/com/sofka/taller/Televisor.java
+++ src/main/java/com/sofka/taller/Televisor.java
@@ -1,4 +1,40 @@
 package com.sofka.taller;
 
 public class Televisor extends Electrodomesticos{
+  private int numeroPulgadas;
+    private String tieneTDT;
+
+    public Televisor(String consumo, String procedencia, int numeroPulgadas, String tieneTDT) {
+        super(consumo, procedencia);
+        this.numeroPulgadas = numeroPulgadas;
+        this.tieneTDT = tieneTDT;
+    }
+
+    public int getNumeroPulgadas() {
+        return numeroPulgadas;
+    }
+
+    public void setNumeroPulgadas(int numeroPulgadas) {
+        this.numeroPulgadas = numeroPulgadas;
+    }
+
+    public String getTieneTDT() {
+        return tieneTDT;
+    }
+
+    public void setTieneTDT(String tieneTDT) {
+        this.tieneTDT = tieneTDT;
+    }
+
+    @Override
+    public double calcularCostoTotal() {
+        double precioAcumulado = super.calcularCostoTotal();
+        if (getNumeroPulgadas() > 40){
+            precioAcumulado += precioAcumulado * 0.30;
+        }
+        if (getTieneTDT().equals("si")){
+            precioAcumulado += 250000;
+        }
+
+    return precioAcumulado;}
 }
